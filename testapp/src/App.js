281da2866cbc070b3b75f59712552de3864b8cd1
Avthar Sewrathan <avthar.s@gmail.com>
2017-12-06 12:15:06 -0500

    Pulled some info from user profile

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -18,7 +18,33 @@ class App extends Component {
   constructor(props){
     super(props);
     //initially set user details to null, will update upon login
-    this.state = {userData: null, userName: 'Wayne Rooney', user: null};
+    
+    let isSignedIn = this.checkSignedInStatus();
+    this.state = {
+      isSignedIn,
+      userData: null,
+      userName: 'Wayne Rooney', 
+      user: isSignedIn && this.loadPerson()
+    };
+  }
+
+  //checks if user is signed in for app state
+  checkSignedInStatus() {
+    if (blockstack.isUserSignedIn()) {
+      // showProfile(profile)
+      return true;
+    } else if (blockstack.isSignInPending()) {
+      blockstack.handlePendingSignIn().then(function(userData) {
+        window.location = window.location.origin
+      })
+      return false;
+    }
+  }
+
+  loadPerson() {
+    let profile = blockstack.loadUserData().profile
+
+    return new blockstack.Person(profile)
   }
 
   //check for login on start
@@ -27,6 +53,8 @@ class App extends Component {
     //  clicked: !prevState.clicked
     //}));
   }
+
+
   render() {
     return (
       <div className="App">
@@ -38,13 +66,20 @@ class App extends Component {
         </p>
         <LoginButton/>
         <hr/>
-        <ProfilePage user = {this.state.user} userData = {this.state.userData} userName = {this.state.userName}/>
+        <ProfilePage user = {this.state.user}
+         userData = {this.state.userData} 
+         userName = {this.state.userName}/>
+         <UserTestComponent
+         user = {this.state.user}
+         />
+
       </div>
     );
   }
 }
 export default App;
 
+//Login Button component
 export class LoginButton extends Component{
   constructor(props){
     super(props);
@@ -53,10 +88,12 @@ export class LoginButton extends Component{
     this.handleClick = this.handleClick.bind(this);
   }
   //onClick: Sign user in in Blockstack browser
-  handleClick(){
+  handleClick(event){
+    event.preventDefault();
     blockstack.redirectToSignIn();
     //BlockTweetSignIn()
   }
+
   render(){
     return(
       <div>
@@ -66,4 +103,25 @@ export class LoginButton extends Component{
       </div>
     );
   }
+}
+
+//Testing user data retrieval from Blockstack Profile
+export class UserTestComponent extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: this.props.user && this.props.user.name() || 'Nameless',
+      avatarUrl: this.props.user && this.props.user.avatarUrl() || undefined
+    };
+  }
+
+  render(){
+    return(
+      <div className="UserInfo">
+      <img class="avatar" src={this.state.avatarUrl} />
+      <h1>{this.state.name}</h1>
+    </div>
+    );
+  }
+
 }
